8fe3908a97f039ea5687580e86720bbc5e22ff5b
shakoor123 <shakoorchachu2001@gmail.com>
2022-12-05 22:32:45 +0530

    persister removed

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,35 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
 
-const persistedReducer = persistReducer(persistConfig, userReducer);
-export let store = () =>
-  configureStore({
-    reducer: {
-      cart: cartReducer,
-      user: persistReducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-        serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        },
-      }),
-  });
-
-export let persistor = persistStore(store);
+export default configureStore({
+  reducer: {
+    cart: cartReducer,
+    user: userReducer,
+  },
+});
